f44d988c81277bfa23201b0fcc8fba6924229be6
Abuzar <abuzarrrakhmonov@gmail.com>
2025-03-02 15:05:07 +0500

    Validatsiya qo'shish

--- routers/lessons.router.js
+++ routers/lessons.router.js
@@ -12,21 +12,27 @@ const adminAuth = require('../middlewares/adminAuth')
 const { body, param, validationResult } = require('express-validator')
 
 router.post('/lessons', adminAuth, [
-    body('title')
-        .isLength({ min: 1 }).withMessage('The lesson title must contain at least one letter'),
-    body('link')
-        .isURL().withMessage('Uncorrect url format'),
+    body("title").notEmpty().withMessage("Title is required"),
+    body("link").isURL().withMessage("Valid link is required"),
+    body("id").optional().custom((value) => {
+        if (value !== null && !/^[0-9a-fA-F]{24}$/.test(value)) {
+            throw new Error("Invalid group ID");
+        }
+        return true;
+    }),
+    body("direction")
+        .if((value, { req }) => !req.body.id)
+        .notEmpty()
+        .withMessage("Direction is required when groupId is null"),
     (req, res, next) => {
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
-            return res.status(400).json({
-                success: false,
-                errors: errors.array()
-            })
+            return res.status(400).json({ success: false, errors: errors.array() })
         }
         next()
     }
 ], newLesson)
+
 router.get('/lessons', adminAuth, getLessons)
 router.get('/lessons/:id', adminAuth, [
     param('id')
