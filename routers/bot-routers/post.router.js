ef4c65459da9f799500ce54f52ded845941f311d
Abuzar <abuzarrrakhmonov@gmail.com>
2025-03-04 21:36:18 +0500

    Fix a small bag

--- routers/bot-routers/post.router.js
+++ routers/bot-routers/post.router.js
@@ -3,7 +3,7 @@ const router = Router()
 const {
     getPosts
 } = require('../../controllers/bot-controllers/post.controller')
-const { verifyAdminToken } = require('../middlewares/authorization')
+const { verifyAdminToken } = require('../../middlewares/authorization')
 
 router.get('/posts', verifyAdminToken, getPosts)
 
